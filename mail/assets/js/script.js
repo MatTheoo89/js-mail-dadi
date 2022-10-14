abffdaa96121f0332ae74a363deaa40eefdf7439
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-14 13:08:57 +0200

    first commit

---
+++ mail/assets/js/script.js
@@ -0,0 +1,7 @@
+// ! CONFRONTO MAIL
+
+// todo Chiedi all’utente la sua email
+
+// todo controlla che sia nella lista di chi può accedere
+
+// todo stampa un messaggio appropriato sull’esito del controllo.
