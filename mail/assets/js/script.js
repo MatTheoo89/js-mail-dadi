0a892a16f209fffa3e9e8e260cbdac35fbcb372b
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-14 15:00:21 +0200

    [add] mail

--- mail/assets/js/script.js
+++ mail/assets/js/script.js
@@ -5,3 +5,43 @@
 // todo controlla che sia nella lista di chi può accedere
 
 // todo stampa un messaggio appropriato sull’esito del controllo.
+
+
+// costante con il prompt input
+// costante array con valori
+// ciclo + if se ci sta il valore
+// stampa in pagina
+
+
+// ! test link
+console.log('====== Sei dentro🤙 ======');
+
+
+// * Dati di partenza
+
+const stamp = document.querySelector('h2');
+
+const userMail = prompt('Inserisci la tua mail');
+
+const listMail = ['mariorossi@mail.com', 'giuseppeverdi@mail.com', 'claudiobianchi@mail.com', 'mariobrown@mail,com'];
+
+let isUserMail = false;
+
+for (let i = 0; i < listMail.length; i++){
+  
+  if(listMail[i] === userMail){
+    isUserMail = true;
+  }
+  // console.log(listMail[i]);
+}
+
+if(isUserMail){
+  stamp.innerText = 'User valido';
+}
+else{
+  stamp.innerText = 'User non valido';
+}
+
+
+
+// ! Seguendo la lezzione di classe ci sono riuscito, a mente mia manco per il...............
